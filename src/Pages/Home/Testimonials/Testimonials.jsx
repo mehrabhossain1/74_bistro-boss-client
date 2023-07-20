ec08c1ffe63e41f4c081c0ef9bb6c25a00871a1e
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-20 15:56:12 +0600

    testimonials p tag

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,13 +1,41 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { useEffect, useState } from "react";
 
 const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
+  //   console.log(reviews);
+
   return (
-    <search>
+    <section className='my-20'>
       <SectionTitle
         subHeading={"What Our Client Say"}
         heading={"Testimonials"}
       ></SectionTitle>
-    </search>
+
+      <Swiper navigation={true} modules={[Navigation]} className='mySwiper'>
+
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className='m-24'>
+              <p>{review.details}</p>
+              <h3 className='text-2xl text-orange-400'>{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
 
