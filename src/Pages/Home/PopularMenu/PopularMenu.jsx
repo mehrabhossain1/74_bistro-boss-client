32a482c24b2a1054532fd821d7a2076b0d7c4874
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-20 19:49:42 +0600

    displayed different menu category items with cover

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -30,6 +30,7 @@ const PopularMenu = () => {
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
+      
       <button className='btn btn-outline border-0 border-b-4 mt-4 text-center '>
         View Full Menu
       </button>
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,22 +1,62 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
 import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
   const [menu] = useMenu();
-  const desert = menu.filter((item) => item.category === "desert");
+
+  const desserts = menu.filter((item) => item.category === "dessert");
   const soup = menu.filter((item) => item.category === "soup");
   const salad = menu.filter((item) => item.category === "salad");
   const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
 
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-
       <Cover img={menuImg} title='our menu'></Cover>
+      {/* main cover */}
+
+      <SectionTitle
+        subHeading={"Don't Miss"}
+        heading={"Today's Offer"}
+      ></SectionTitle>
+
+      {/* offered menu items */}
+      <MenuCategory items={offered}></MenuCategory>
+
+      {/* desert menu items */}
+      <MenuCategory
+        items={desserts}
+        title={"Desserts"}
+        img={dessertImg}
+      ></MenuCategory>
+
+      {/* pizza */}
+      <MenuCategory
+        items={pizza}
+        title={"Pizzas"}
+        img={pizzaImg}
+      ></MenuCategory>
+
+      {/* salads */}
+      <MenuCategory
+        items={salad}
+        title={"Salads"}
+        img={saladImg}
+      ></MenuCategory>
+
+      {/* soups */}
+      <MenuCategory items={soup} title={"Soups"} img={soupImg}></MenuCategory>
     </div>
   );
 };
