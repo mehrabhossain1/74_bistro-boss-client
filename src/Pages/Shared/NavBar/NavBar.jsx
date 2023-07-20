5eab277ed8e42240e4284357fad8a653517881e5
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-20 18:00:00 +0600

    used react helmet

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,22 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   const navOptions = (
     <>
       <li>
-        <a>Item 1</a>
+        <Link to='/'>Home</Link>
       </li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className='p-2'>
-            <li>
-              <a>Submenu 1</a>
-            </li>
-            <li>
-              <a>Submenu </a>
-            </li>
-          </ul>
-        </details>
+      <li>
+        <Link to='/menu'>Our Menu</Link>
       </li>
+
       <li>
         <a>Item 3</a>
       </li>
