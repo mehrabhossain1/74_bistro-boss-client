99f6078ca83f61dc8d207ff17a6f2915f64fd12f
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-22 00:53:16 +0600

    signup validation

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -4,15 +4,12 @@ const SignUp = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
   };
-
-  console.log(watch("example"));
 
   return (
     <>
@@ -36,11 +33,14 @@ const SignUp = () => {
                 </label>
                 <input
                   type='text'
-                  {...register("name")}
+                  {...register("name", { required: true })}
                   name='name'
                   placeholder='name'
                   className='input input-bordered'
                 />
+                {errors.name && (
+                  <span className='text-red-600'>Name is required</span>
+                )}
               </div>
               <div className='form-control'>
                 <label className='label'>
@@ -48,11 +48,14 @@ const SignUp = () => {
                 </label>
                 <input
                   type='email'
-                  {...register("email")}
+                  {...register("email", { required: true })}
                   name='email'
                   placeholder='email'
                   className='input input-bordered'
                 />
+                {errors.email && (
+                  <span className='text-red-600'>Email is required</span>
+                )}
               </div>
               <div className='form-control'>
                 <label className='label'>
@@ -60,11 +63,18 @@ const SignUp = () => {
                 </label>
                 <input
                   type='password'
-                  {...register("password")}
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                  })}
                   name='password'
                   placeholder='password'
                   className='input input-bordered'
                 />
+                {errors.password?.type === "required" && (
+                  <p className='text-red-600'>Password is required</p>
+                )}
                 <label className='label'>
                   <a href='#' className='label-text-alt link link-hover'>
                     Forgot password?
