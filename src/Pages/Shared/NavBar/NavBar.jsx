94a6a6d9e206038273ace4ac8fa3e2f243bc6056
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-25 11:37:56 +0600

    dashboard user

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import { FaShoppingCart } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
 
 const NavBar = () => {
   const { user, logOut } = useContext(AuthContext);
+  const [cart] = useCart();
 
   const handleLogout = () => {
     logOut()
@@ -30,7 +32,7 @@ const NavBar = () => {
         <Link to='/'>
           <button className='btn'>
             <FaShoppingCart />
-            <div className='badge badge-secondary'>+99</div>
+            <div className='badge badge-secondary'>+{cart?.length || 0}</div>
           </button>
         </Link>
       </li>
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,10 +2,12 @@ import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
+import useCart from "../../hooks/useCart";
 
 const FoodCard = ({ item }) => {
   const { name, image, price, recipe, _id } = item;
   const { user } = useContext(AuthContext);
+  const [, refetch] = useCart();
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -30,6 +32,7 @@ const FoodCard = ({ item }) => {
         .then((res) => res.json())
         .then((data) => {
           if (data.insertedId) {
+            refetch(); // refetch cart to update the number of items in the cart
             Swal.fire({
               position: "top-end",
               icon: "success",
