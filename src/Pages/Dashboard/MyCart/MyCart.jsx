0aa1f2432306b4f0f10eb28e0efa9a9e1f8b7fbd
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-25 14:57:34 +0600

    mycart designed

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyCart = () => {
   const [cart] = useCart();
@@ -10,10 +11,48 @@ const MyCart = () => {
       <Helmet>
         <title>Bistro Boss | My Cart</title>
       </Helmet>
-      <div className='uppercase'>
+      <div className='uppercase h-[60px] font-semibold flex justify-evenly items-center'>
         <h3 className='text-3xl'>Total Items: {cart.length}</h3>
         <h3 className='text-3xl'>Total Price: ${total}</h3>
         <button className='btn btn-warning btn-sm'>PAY</button>
+      </div>
+      <div className='overflow-x-auto'>
+        <table className='table'>
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Food</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className='avatar'>
+                    <div className='mask mask-squircle w-12 h-12'>
+                      <img
+                        src={item.image}
+                        alt='Avatar Tailwind CSS Component'
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td className='text-end'>${item.price}</td>
+                <td>
+                  <button className='btn btn-ghost btn-xm bg-red-600 text-white'>
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
