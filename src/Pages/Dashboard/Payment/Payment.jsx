9e08ebc9c6f6fd71ff36c04ce27ac3ac3684f632
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-28 02:23:48 +0600

    payment integration

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,4 +1,10 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+
+// TODO: provide publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
   return (
@@ -7,7 +13,12 @@ const Payment = () => {
         subHeading='Please Process'
         heading='Payment'
       ></SectionTitle>
+
       <h2 className='text-3xl'>teka pakhi</h2>
+
+      <Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
     </div>
   );
 };
