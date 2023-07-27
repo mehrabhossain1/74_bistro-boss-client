43e38b24a29d2478a456b3094c69767924d7d59e
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-28 00:57:18 +0600

    payment route created

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Payment = () => {
+  return (
+    <div>
+      <SectionTitle
+        subHeading='Please Process'
+        heading='Payment'
+      ></SectionTitle>
+      <h2 className='text-3xl'>teka pakhi</h2>
+    </div>
+  );
+};
+
+export default Payment;
