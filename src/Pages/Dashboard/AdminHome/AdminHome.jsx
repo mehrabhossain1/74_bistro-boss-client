18d8099dc292d7a4b5ef0ea0fc9019df07ee2659
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-28 15:22:30 +0600

    admin-stats data load

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,7 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const AdminHome = () => {
   const { user } = useAuth();
+const [axiosSecure] = useAxiosSecure()
+
+
+    const {data: stats={} } = useQuery({
+        queryKey: ["admin-stats"],
+        queryFn: async()=>{
+            const res = await axiosSecure("/admin-stats");
+            return res.data;
+        }
+    })
+
   return (
     <div className='w-full m-4'>
       <h2 className='text-3xl'>Hi, {user.displayName}</h2>
